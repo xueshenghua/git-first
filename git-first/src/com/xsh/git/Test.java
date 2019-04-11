81875fc975a4465ad93d9e555bbc40c5f65a4fa1
xueshenghua <xueshh@yonyou.com>
2019-04-11 09:34:29 +0800

    修改test.java方法

--- git-first/src/com/xsh/git/Test.java
+++ git-first/src/com/xsh/git/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git...");
     }
+
+    public void test() {
+        System.out.println("test compare");
+    }
 }
